122dcb95c53676403a1bab042f434595ee49d58a
alexander <xmedalex@yandex.ru>
2023-05-22 19:33:47 +0300

    second commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Blod() {
+  return <h1>Blog</h1>;
+}
---
+++ app/components/TheFooter.tsx
@@ -0,0 +1,9 @@
+const TheFooter = () => {
+    return (
+        <footer className="container">
+            Created by &copy;AlexanderI
+        </footer>
+    )
+};
+
+export { TheFooter }
